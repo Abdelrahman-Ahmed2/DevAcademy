7ae570e13196ccd9481f32583b8a19467ee7de5d
Abdelrahman-Ahmed2 <152915910+Abdelrahman-Ahmed2@users.noreply.github.com>
2026-05-28 23:05:01 +0300

    Delete scripts/script.js

--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-var $grid = $(' .xgrid').isotope({});
-$('.filter-button-group').on('click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({filter: filterValue})
-});
